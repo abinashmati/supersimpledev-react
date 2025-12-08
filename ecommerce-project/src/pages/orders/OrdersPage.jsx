b531fe8d3252ebf758cecea09094fe36e0177f7b
abinashmati@gmail.com <abinashmati@gmail.com>
2025-12-08 10:28:15 +0000

    seperated into smaller components

--- ecommerce-project/src/pages/orders/OrdersPage.jsx
+++ ecommerce-project/src/pages/orders/OrdersPage.jsx
@@ -2,10 +2,10 @@ import axios from "axios";
 import { useState, useEffect, Fragment } from "react";
 import { NavLink } from "react-router";
 import dayjs from "dayjs";
-import { formatMoney } from "../utils/money";
-import { Header } from "../components/Header";
+import { formatMoney } from "../../utils/money";
+import { Header } from "../../components/Header";
 
-import BuyAgainIcon from "../assets/images/icons/buy-again.png";
+import BuyAgainIcon from "../../assets/images/icons/buy-again.png";
 
 import "./OrdersPage.css";
 
