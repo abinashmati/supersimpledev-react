eaf39505597124bcc0c5e74c6a4a5aa948311ce8
abinashmati@gmail.com <abinashmati@gmail.com>
2025-12-09 10:08:33 +0000

    display 'Added' message below products in product.jsx in home page

--- ecommerce-project/src/pages/home/Product.jsx
+++ ecommerce-project/src/pages/home/Product.jsx
@@ -5,6 +5,7 @@ import CheckMarkIcon from "../../assets/images/icons/checkmark.png";
 
 export function Product({ product, loadCart }) {
   const [quantity, setQuantity] = useState(1);
+  const [showAddedMessage, setshowAddedMessage] = useState(false);
 
   const addToCart = async () => {
     await axios.post("/api/cart-items", {
@@ -13,6 +14,13 @@ export function Product({ product, loadCart }) {
     });
 
     await loadCart(); //reload the cart data with the new saved quantity
+
+    setshowAddedMessage(true); // we set it to true so that we can show the added message with green tick
+
+    setTimeout(() => {
+      setshowAddedMessage(false);
+      //after 3 seconds we will update the state to false again so that we will not show the added taxt with check mark. we will only dipaly it for 3 seconds once a product it added to cart by the Add to Cart button click
+    }, 3000);
   };
 
   const selectQuantity = (event) => {
@@ -58,7 +66,10 @@ export function Product({ product, loadCart }) {
 
       <div className="product-spacer"></div>
 
-      <div className="added-to-cart">
+      <div
+        className="added-to-cart"
+        style={{ opacity: showAddedMessage ? 1 : 0 }}
+      >
         <img src={CheckMarkIcon} />
         Added
       </div>
