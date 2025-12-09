753da9b39b07d0a89382938fd62baedf4af4df5b
abinashmati@gmail.com <abinashmati@gmail.com>
2025-12-09 09:47:13 +0000

    Add items to cart form order page

--- ecommerce-project/src/pages/orders/OrderDetailsGrid.jsx
+++ ecommerce-project/src/pages/orders/OrderDetailsGrid.jsx
@@ -1,12 +1,22 @@
 import { Fragment } from "react";
 import { NavLink } from "react-router";
+import axios from "axios";
 import dayjs from "dayjs";
 import BuyAgainIcon from "../../assets/images/icons/buy-again.png";
 
-export function OrderDetailsGrid({ order }) {
+export function OrderDetailsGrid({ order, loadCart }) {
   return (
     <div className="order-details-grid">
       {order.products.map((orderProduct) => {
+        const addToCart = async () => {
+          await axios.post("/api/cart-items", {
+            productId: orderProduct.product.id,
+            quantity: 1,
+          });
+
+          loadCart(); // after adding items into cart we must reload the cart so that all the components with new values and checkout page will be reloaded with the new cart item
+        };
+
         return (
           <Fragment key={orderProduct.product.id}>
             <div className="product-image-container">
@@ -24,7 +34,9 @@ export function OrderDetailsGrid({ order }) {
               </div>
               <button className="buy-again-button button-primary">
                 <img className="buy-again-icon" src={BuyAgainIcon} />
-                <span className="buy-again-message">Add to Cart</span>
+                <span className="buy-again-message" onClick={addToCart}>
+                  Add to Cart
+                </span>
               </button>
             </div>
 
--- ecommerce-project/src/pages/orders/OrdersPage.jsx
+++ ecommerce-project/src/pages/orders/OrdersPage.jsx
@@ -5,7 +5,7 @@ import { OrdersGrid } from "./OrdersGrid";
 import { Header } from "../../components/Header";
 import "./OrdersPage.css";
 
-export function OrdersPage({ cart }) {
+export function OrdersPage({ cart, loadCart }) {
   const [orders, setOrders] = useState([]);
 
   useEffect(() => {
@@ -27,7 +27,7 @@ export function OrdersPage({ cart }) {
       <div className="orders-page">
         <div className="page-title">Your Orders</div>
 
-        <OrdersGrid orders={orders} />
+        <OrdersGrid orders={orders} loadCart={loadCart} />
       </div>
     </>
   );
