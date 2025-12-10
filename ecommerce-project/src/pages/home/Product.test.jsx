1060c4df63f0af545272febea9a9d138afe29eb6
abinashmati@gmail.com <abinashmati@gmail.com>
2025-12-10 10:29:06 +0000

    test case for click Add to Cart button in product component

--- ecommerce-project/src/pages/home/Product.test.jsx
+++ ecommerce-project/src/pages/home/Product.test.jsx
@@ -1,6 +1,10 @@
 import { it, expect, describe, vi } from "vitest";
 import { render, screen } from "@testing-library/react";
+import userEvent from "@testing-library/user-event";
 import { Product } from "./Product";
+import axios from "axios";
+
+vi.mock("axios");
 
 describe("Product Component", () => {
   it("displays the product details correctly", () => {
@@ -42,5 +46,37 @@ describe("Product Component", () => {
 
     expect(screen.getByText(product.rating.count)).toBeInTheDocument();
     //expect(screen.getByText("87")).toBeInTheDocument();
+  });
+
+  //Test user interaction by button click
+  it("add a product to the cart", async () => {
+    const product = {
+      id: "e43638ce-6aa0-4b85-b27f-e1d07eb678c6",
+      image: "images/products/athletic-cotton-socks-6-pairs.jpg",
+      name: "Black and Gray Athletic Cotton Socks - 6 Pairs",
+      rating: {
+        stars: 4.5,
+        count: 87,
+      },
+      priceCents: 1090,
+      keywords: ["socks", "sports", "apparel"],
+    };
+    const loadCart = vi.fn();
+
+    render(<Product product={product} loadCart={loadCart} />);
+
+    //setup user event now we can simulate a click event and in the click fucntion we will let it know which element to click.
+    //now we will give a data-testid proerty with value what ever we want like we gave in this case 'add-to-cart-button' in the product component , then we will get that element based on the data-testid by using screen then will save it to a varibale and pass the element to user.click function.
+    //so that it will simulate a click event on that element.
+    const user = userEvent.setup();
+    const addTocartButton = screen.getByTestId("add-to-cart-button");
+    await user.click(addTocartButton);
+
+    expect(axios.post).toHaveBeenCalledWith("/api/cart-items", {
+      productId: "e43638ce-6aa0-4b85-b27f-e1d07eb678c6",
+      quantity: 1,
+    });
+
+    expect(loadCart).toHaveBeenCalled();
   });
 });
